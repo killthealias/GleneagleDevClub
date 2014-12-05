b79b10714fdbbaafac4c70fa28c8a8572f715c93
Polunom <kelvinzdragon@gmail.com>
2014-12-04 17:26:03 -0800

    Changed project layout
    - no longer has blue, background
    - no longer has parallax scrolling
    - fixed screen at 4:3 (800x600)
    - added background from dropbox

--- Assets/Sample Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Assets/Sample Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -2,6 +2,7 @@
 
 public class PlatformerCharacter2D : MonoBehaviour 
 {
+	private float minX, maxX, minY, maxY;
 	bool facingRight = true;							// For determining which way the player is currently facing.
 
 	[SerializeField] float maxSpeed = 10f;				// The fastest the player can travel in the x axis.
@@ -29,9 +30,33 @@ public class PlatformerCharacter2D : MonoBehaviour
 		anim = GetComponent<Animator>();
 	}
 
+	void Start(){
+		// If you want the min max values to update if the resolution changes 
+		// set them in update else set them in Start
+		float camDistance = Vector3.Distance(transform.position, Camera.main.transform.position);
+		Vector2 bottomCorner = Camera.main.ViewportToWorldPoint(new Vector3(0,0, camDistance));
+		Vector2 topCorner = Camera.main.ViewportToWorldPoint(new Vector3(1,1, camDistance));
+		
+		minX = bottomCorner.x;
+		maxX = topCorner.x;
+		minY = bottomCorner.y;
+		maxY = topCorner.y;
+	}
 
 	void FixedUpdate()
 	{
+		Vector3 pos = transform.position;
+		
+		// Horizontal contraint
+		if(pos.x < minX) pos.x = minX;
+		if(pos.x > maxX) pos.x = maxX;
+		
+		// vertical contraint
+		if(pos.y < minY) pos.y = minY;
+		if(pos.y > maxY) pos.y = maxY;
+		
+		// Update position
+		transform.position = pos;
 		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
 		grounded = Physics2D.OverlapCircle(groundCheck.position, groundedRadius, whatIsGround);
 		anim.SetBool("Ground", grounded);
