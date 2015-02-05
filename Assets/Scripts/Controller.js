34f342651a6a18350405263d4868f482eaef1259
killthealias <killthealias@gmail.com>
2015-02-04 22:16:45 -0800

    Added Proper Jumping Animation

--- Assets/Scripts/Controller.js
+++ Assets/Scripts/Controller.js
@@ -12,17 +12,4 @@ function FixedUpdate () {
 	if (Input.GetKey("right")) {
 		transform.position.x += 0.1;
 	}
-	if (Input.GetKey("up") && is_jumping == false) {
-		transform.position.y += 2;
-		is_jumping = true;
-		is_jumping_reset = 0;
-	}
-	if (is_jumping == true) {
-		if (is_jumping_reset == 35) {
-			is_jumping = false;
-		}
-		else {
-			is_jumping_reset += 1;
-		}
-	}
 }
---
+++ Assets/Scripts/JumpScript.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+var is_jumping : boolean;
+var is_jumping_reset : int;
+
+function FixedUpdate () {
+	if (Input.GetKey("up") && is_jumping == false) {
+		animation.Play();
+		is_jumping = true;
+		is_jumping_reset = 0;
+	}
+	if (is_jumping == true) {
+		if (is_jumping_reset == 35) {
+			is_jumping = false;
+		}
+		else {
+			is_jumping_reset += 1;
+		}
+	}
+}
